6369d664651f4b28f40b2c2950fd5b804d4ee366
Sangho Kim <marchbreeze03@gmail.com>
2024-10-27 01:04:57 +0900

    [REFACTOR/#153] 프롬프트뷰 UI 및 기능 전면 수정

--- presentation/src/main/java/kr/genti/presentation/create/DefineFragment.kt
+++ presentation/src/main/java/kr/genti/presentation/create/DefineFragment.kt
@@ -16,7 +16,6 @@ import coil.load
 import dagger.hilt.android.AndroidEntryPoint
 import kr.genti.core.base.BaseFragment
 import kr.genti.core.extension.getFileName
-import kr.genti.core.extension.initOnBackPressedListener
 import kr.genti.core.extension.setOnSingleClickListener
 import kr.genti.core.extension.toast
 import kr.genti.domain.entity.response.ImageFileModel
@@ -31,8 +30,6 @@ import kr.genti.presentation.util.AmplitudeManager.PROPERTY_PAGE
 @AndroidEntryPoint
 class DefineFragment() : BaseFragment<FragmentDefineBinding>(R.layout.fragment_define) {
     private val viewModel by activityViewModels<CreateViewModel>()
-    private lateinit var photoPickerResult: ActivityResultLauncher<PickVisualMediaRequest>
-    private lateinit var galleryPickerResult: ActivityResultLauncher<Intent>
 
     override fun onViewCreated(
         view: View,
@@ -43,15 +40,6 @@ class DefineFragment() : BaseFragment<FragmentDefineBinding>(R.layout.fragment_d
         initView()
         initCreateBtnListener()
         initRefreshExBtnListener()
-        initAddImageBtnListener()
-        initDeleteBtnListener()
-        setGalleryImageWithPhotoPicker()
-        setGalleryImageWithGalleryPicker()
-    }
-
-    override fun onResume() {
-        super.onResume()
-        setSavedImage()
     }
 
     private fun initView() {
@@ -82,75 +70,6 @@ class DefineFragment() : BaseFragment<FragmentDefineBinding>(R.layout.fragment_d
                 plusIntProperties("user_promptsuggest_refresh")
             }
             binding.tvCreateRandomExample.text = viewModel.getRandomPrompt()
-        }
-    }
-
-    private fun initAddImageBtnListener() {
-        with(binding) {
-            btnCreatePlus.setOnSingleClickListener { checkAndGetImages() }
-            layoutPlusImage.setOnSingleClickListener { checkAndGetImages() }
-        }
-    }
-
-    private fun initDeleteBtnListener() {
-        binding.btnDeleteImage.setOnSingleClickListener {
-            viewModel.plusImage = emptyImageFileModel()
-            binding.layoutPlusImage.isVisible = false
-            binding.btnDeleteImage.isVisible = false
-        }
-    }
-
-    private fun checkAndGetImages() {
-        if (isPhotoPickerAvailable(requireContext())) {
-            photoPickerResult.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
-        } else {
-            galleryPickerResult.launch(
-                Intent(Intent.ACTION_PICK).apply { type = "image/*" },
-            )
-        }
-    }
-
-    private fun setGalleryImageWithPhotoPicker() {
-        photoPickerResult =
-            registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
-                if (uri != null) setImageWithUri(uri)
-            }
-    }
-
-    private fun setGalleryImageWithGalleryPicker() {
-        galleryPickerResult =
-            registerForActivityResult(
-                ActivityResultContracts.StartActivityForResult(),
-            ) { result ->
-                when (result.resultCode) {
-                    Activity.RESULT_OK -> result.data?.data?.let { setImageWithUri(it) }
-                    Activity.RESULT_CANCELED -> return@registerForActivityResult
-                    else -> toast(getString(R.string.selfie_toast_picker_error))
-                }
-            }
-    }
-
-    private fun setImageWithUri(uri: Uri) {
-        viewModel.plusImage =
-            ImageFileModel(
-                uri.hashCode().toLong(),
-                uri.getFileName(requireActivity().contentResolver).toString(),
-                uri.toString(),
-            )
-        with(binding) {
-            ivAddedImage.load(uri)
-            layoutPlusImage.isVisible = true
-            btnDeleteImage.isVisible = true
-        }
-    }
-
-    private fun setSavedImage() {
-        if (viewModel.plusImage.id != (-1).toLong()) {
-            with(binding) {
-                ivAddedImage.load(viewModel.plusImage.url)
-                layoutPlusImage.isVisible = true
-                btnDeleteImage.isVisible = true
-            }
         }
     }
 }
