a7094c1880fa2fb2f77d7dcd2bce59e9ce6c3d70
Sangho Kim <marchbreeze03@gmail.com>
2024-10-28 16:15:46 +0900

    [ADD/#155] 텍스트뷰 앞 이미지 표시 확장함수 추가

--- core/src/main/java/kr/genti/core/extension/ViewExt.kt
+++ core/src/main/java/kr/genti/core/extension/ViewExt.kt
@@ -4,6 +4,9 @@ import android.graphics.LinearGradient
 import android.graphics.RenderEffect
 import android.graphics.Shader
 import android.os.Build
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ImageSpan
 import android.view.View
 import android.widget.TextView
 
@@ -43,4 +46,11 @@ fun TextView.setGradientText(startColor: Int, endColor: Int) {
         null,
         Shader.TileMode.CLAMP
     )
+}
+
+fun TextView.setTextWithImage(text: String, imageResId: Int) {
+    val spannableString = SpannableString("  $text")
+    val imageSpan = ImageSpan(context, imageResId, ImageSpan.ALIGN_BOTTOM)
+    spannableString.setSpan(imageSpan, 0, 1, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+    this.text = spannableString
 }
