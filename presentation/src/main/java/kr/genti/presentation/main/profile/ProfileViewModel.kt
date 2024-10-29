534caab411e3695a2b3890b003761097793fe7e2
Sangho Kim <marchbreeze03@gmail.com>
2024-10-30 04:10:39 +0900

    [FIX/#157] 프로필뷰 생성 사진 추가 버튼 API 고려

--- presentation/src/main/java/kr/genti/presentation/main/profile/ProfileViewModel.kt
+++ presentation/src/main/java/kr/genti/presentation/main/profile/ProfileViewModel.kt
@@ -8,66 +8,92 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import kr.genti.core.state.UiState
 import kr.genti.domain.entity.response.PicturePagedListModel
+import kr.genti.domain.entity.response.ServerAvailableModel
 import kr.genti.domain.enums.GenerateStatus
 import kr.genti.domain.repository.GenerateRepository
 import javax.inject.Inject
 
 @HiltViewModel
 class ProfileViewModel
-    @Inject
-    constructor(
-        private val generateRepository: GenerateRepository,
-    ) : ViewModel() {
-        private val _getGenerateStatusState =
-            MutableStateFlow<UiState<Boolean>>(UiState.Empty)
-        val getGenerateStatusState: StateFlow<UiState<Boolean>> = _getGenerateStatusState
+@Inject
+constructor(
+    private val generateRepository: GenerateRepository,
+) : ViewModel() {
+    private val _getGenerateStatusState =
+        MutableStateFlow<UiState<Boolean>>(UiState.Empty)
+    val getGenerateStatusState: StateFlow<UiState<Boolean>> = _getGenerateStatusState
 
-        private val _getPictureListState =
-            MutableStateFlow<UiState<PicturePagedListModel>>(UiState.Loading)
-        val getPictureListState: StateFlow<UiState<PicturePagedListModel>> = _getPictureListState
+    var currentStatus: GenerateStatus = GenerateStatus.NEW_REQUEST_AVAILABLE
 
-        private var currentPage = -1
-        private var isPagingFinish = false
-        private var totalPage = Int.MAX_VALUE
+    private val _serverAvailableState =
+        MutableStateFlow<UiState<ServerAvailableModel>>(UiState.Empty)
+    val serverAvailableState: StateFlow<UiState<ServerAvailableModel>> = _serverAvailableState
 
-        var isFirstLoading = true
+    private val _getPictureListState =
+        MutableStateFlow<UiState<PicturePagedListModel>>(UiState.Loading)
+    val getPictureListState: StateFlow<UiState<PicturePagedListModel>> = _getPictureListState
 
-        fun getGenerateStatusFromServer() {
-            viewModelScope.launch {
-                _getGenerateStatusState.value = UiState.Loading
-                generateRepository.getGenerateStatus()
-                    .onSuccess {
-                        _getGenerateStatusState.value =
-                            UiState.Success(it.status == GenerateStatus.IN_PROGRESS)
-                    }
-                    .onFailure { t ->
-                        _getGenerateStatusState.value = UiState.Failure(t.message.toString())
-                    }
-            }
-        }
+    private var currentPage = -1
+    private var isPagingFinish = false
+    private var totalPage = Int.MAX_VALUE
 
-        fun getPictureListFromServer() {
-            viewModelScope.launch {
-                if (isPagingFinish) return@launch
-                _getPictureListState.value = UiState.Loading
-                generateRepository.getGeneratedPictureList(
-                    ++currentPage,
-                    10,
-                    null,
-                    null,
-                )
-                    .onSuccess {
-                        totalPage = it.totalPages - 1
-                        if (totalPage == currentPage) isPagingFinish = true
-                        if (it.content.isEmpty()) {
-                            _getPictureListState.value = UiState.Empty
-                            return@launch
-                        }
-                        if (isFirstLoading) isFirstLoading = false
-                        _getPictureListState.value = UiState.Success(it)
-                    }.onFailure {
-                        _getPictureListState.value = UiState.Failure(it.message.toString())
-                    }
-            }
+    var isFirstLoading = true
+
+    fun getGenerateStatusFromServer() {
+        viewModelScope.launch {
+            _getGenerateStatusState.value = UiState.Loading
+            generateRepository.getGenerateStatus()
+                .onSuccess {
+                    currentStatus = it.status
+                    _getGenerateStatusState.value =
+                        UiState.Success(it.status == GenerateStatus.IN_PROGRESS)
+
+                }
+                .onFailure { t ->
+                    _getGenerateStatusState.value = UiState.Failure(t.message.toString())
+                }
         }
     }
+
+    fun getIsServerAvailable() {
+        _serverAvailableState.value = UiState.Loading
+        viewModelScope.launch {
+            generateRepository
+                .getIsServerAvailable()
+                .onSuccess {
+                    _serverAvailableState.value = UiState.Success(it)
+                }.onFailure {
+                    _serverAvailableState.value = UiState.Failure(it.message.orEmpty())
+                }
+        }
+    }
+
+    fun resetIsServerAvailable() {
+        _serverAvailableState.value = UiState.Empty
+    }
+
+    fun getPictureListFromServer() {
+        viewModelScope.launch {
+            if (isPagingFinish) return@launch
+            _getPictureListState.value = UiState.Loading
+            generateRepository.getGeneratedPictureList(
+                ++currentPage,
+                10,
+                null,
+                null,
+            )
+                .onSuccess {
+                    totalPage = it.totalPages - 1
+                    if (totalPage == currentPage) isPagingFinish = true
+                    if (it.content.isEmpty()) {
+                        _getPictureListState.value = UiState.Empty
+                        return@launch
+                    }
+                    if (isFirstLoading) isFirstLoading = false
+                    _getPictureListState.value = UiState.Success(it)
+                }.onFailure {
+                    _getPictureListState.value = UiState.Failure(it.message.toString())
+                }
+        }
+    }
+}
