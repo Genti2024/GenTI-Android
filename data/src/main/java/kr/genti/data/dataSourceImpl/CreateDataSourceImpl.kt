c775abacbd5e546d8596176e432a15b1672bb497
Sangho Kim <marchbreeze03@gmail.com>
2024-10-31 19:16:54 +0900

    [ADD/#163] 프롬프트 예시 조회 API data 모듈 설정

--- data/src/main/java/kr/genti/data/dataSourceImpl/CreateDataSourceImpl.kt
+++ data/src/main/java/kr/genti/data/dataSourceImpl/CreateDataSourceImpl.kt
@@ -5,21 +5,28 @@ import kr.genti.data.dto.BaseResponse
 import kr.genti.data.dto.request.CreateRequestDto
 import kr.genti.data.dto.request.KeyRequestDto
 import kr.genti.data.dto.request.S3RequestDto
+import kr.genti.data.dto.response.PromptExampleDto
 import kr.genti.data.dto.response.S3PresignedUrlDto
 import kr.genti.data.service.CreateService
 import javax.inject.Inject
 
 data class CreateDataSourceImpl
-    @Inject
-    constructor(
-        private val createService: CreateService,
-    ) : CreateDataSource {
-        override suspend fun getSingleS3Url(request: S3RequestDto): BaseResponse<S3PresignedUrlDto> = createService.getSingleS3Url(request)
+@Inject
+constructor(
+    private val createService: CreateService,
+) : CreateDataSource {
+    override suspend fun getSingleS3Url(request: S3RequestDto): BaseResponse<S3PresignedUrlDto> =
+        createService.getSingleS3Url(request)
 
-        override suspend fun getMultiS3Url(request: List<S3RequestDto>): BaseResponse<List<S3PresignedUrlDto>> =
-            createService.getMultiS3Url(request)
+    override suspend fun getMultiS3Url(request: List<S3RequestDto>): BaseResponse<List<S3PresignedUrlDto>> =
+        createService.getMultiS3Url(request)
 
-        override suspend fun postToCreate(request: CreateRequestDto): BaseResponse<Boolean> = createService.postToCreate(request)
+    override suspend fun postToCreate(request: CreateRequestDto): BaseResponse<Boolean> =
+        createService.postToCreate(request)
 
-        override suspend fun postToVerify(request: KeyRequestDto): BaseResponse<Boolean> = createService.postToVerify(request)
-    }
+    override suspend fun postToVerify(request: KeyRequestDto): BaseResponse<Boolean> =
+        createService.postToVerify(request)
+
+    override suspend fun getPromptExample(): BaseResponse<List<PromptExampleDto>> =
+        createService.getPromptExample()
+}
--- data/src/main/java/kr/genti/data/repositoryImpl/CreateRepositoryImpl.kt
+++ data/src/main/java/kr/genti/data/repositoryImpl/CreateRepositoryImpl.kt
@@ -7,32 +7,38 @@ import kr.genti.data.dto.request.S3RequestDto.Companion.toDto
 import kr.genti.domain.entity.request.CreateRequestModel
 import kr.genti.domain.entity.request.KeyRequestModel
 import kr.genti.domain.entity.request.S3RequestModel
+import kr.genti.domain.entity.response.PromptExampleModel
 import kr.genti.domain.entity.response.S3PresignedUrlModel
 import kr.genti.domain.repository.CreateRepository
 import javax.inject.Inject
 
 class CreateRepositoryImpl
-    @Inject
-    constructor(
-        private val createDataSource: CreateDataSource,
-    ) : CreateRepository {
-        override suspend fun getS3SingleUrl(request: S3RequestModel): Result<S3PresignedUrlModel> =
-            runCatching {
-                createDataSource.getSingleS3Url(request.toDto()).response.toModel()
-            }
+@Inject
+constructor(
+    private val createDataSource: CreateDataSource,
+) : CreateRepository {
+    override suspend fun getS3SingleUrl(request: S3RequestModel): Result<S3PresignedUrlModel> =
+        runCatching {
+            createDataSource.getSingleS3Url(request.toDto()).response.toModel()
+        }
 
-        override suspend fun getS3MultiUrl(request: List<S3RequestModel>): Result<List<S3PresignedUrlModel>> =
-            runCatching {
-                createDataSource.getMultiS3Url(request.map { it.toDto() }).response.map { it.toModel() }
-            }
+    override suspend fun getS3MultiUrl(request: List<S3RequestModel>): Result<List<S3PresignedUrlModel>> =
+        runCatching {
+            createDataSource.getMultiS3Url(request.map { it.toDto() }).response.map { it.toModel() }
+        }
 
-        override suspend fun postToCreate(request: CreateRequestModel): Result<Boolean> =
-            runCatching {
-                createDataSource.postToCreate(request.toDto()).response
-            }
+    override suspend fun postToCreate(request: CreateRequestModel): Result<Boolean> =
+        runCatching {
+            createDataSource.postToCreate(request.toDto()).response
+        }
 
-        override suspend fun postToVerify(request: KeyRequestModel): Result<Boolean> =
-            runCatching {
-                createDataSource.postToVerify(request.toDto()).response
-            }
-    }
+    override suspend fun postToVerify(request: KeyRequestModel): Result<Boolean> =
+        runCatching {
+            createDataSource.postToVerify(request.toDto()).response
+        }
+
+    override suspend fun getPromptExample(): Result<List<PromptExampleModel>> =
+        runCatching {
+            createDataSource.getPromptExample().response.map { it.toModel() }
+        }
+}
