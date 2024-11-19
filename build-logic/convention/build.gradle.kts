f89f045da7f4816eb6100c7263b920321bb76be5
Sangho Kim <marchbreeze03@gmail.com>
2024-11-20 04:55:02 +0900

    [ADD/#171] convention 모듈 build gradle 설정

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -1,46 +1,13 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    `kotlin-dsl`
 }
 
-android {
-    namespace = "kr.genti.convention"
-    compileSdk = 34
-
-    defaultConfig {
-        applicationId = "kr.genti.convention"
-        minSdk = 28
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
-    }
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material.design)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test)
-    androidTestImplementation(libs.espresso)
+    compileOnly(libs.android.application)
+    compileOnly(libs.kotlin.android)
 }
