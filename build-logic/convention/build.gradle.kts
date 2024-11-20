44b55b6d3b0efa4ff4fbac945c38af2a0005dd2b
Sangho Kim <marchbreeze03@gmail.com>
2024-11-20 23:00:26 +0900

    [ADD/#171] build Gradle에 플러그인 등록

--- build-logic/convention/build.gradle.kts
+++ build-logic/convention/build.gradle.kts
@@ -11,3 +11,20 @@ dependencies {
     compileOnly(libs.android.gradlePlugin)
     compileOnly(libs.kotlin.gradlePlugin)
 }
+
+gradlePlugin {
+    plugins {
+        register("androidApplication") {
+            id = "kr.genti.androidApplication"
+            implementationClass = "kr.genti.buildlogic.AndroidApplicationPlugin"
+        }
+        register("androidLibrary") {
+            id = "kr.genti.androidLibrary"
+            implementationClass = "kr.genti.buildlogic.AndroidLibraryPlugin"
+        }
+        register("javaLibrary") {
+            id = "kr.genti.javaLibrary"
+            implementationClass = "kr.genti.buildlogic.JavaLibraryPlugin"
+        }
+    }
+}
