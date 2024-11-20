468161a02fdb32c35064c397af9e7f1f35f8db65
Sangho Kim <marchbreeze03@gmail.com>
2024-11-20 21:59:02 +0900

    [ADD/#171] project configuration 설정

--- build-logic/convention/src/main/java/kr/genti/convention/Constants.kt
+++ build-logic/convention/src/main/java/kr/genti/convention/Constants.kt
@@ -2,10 +2,13 @@ package kr.genti.convention
 
 object Constants {
     const val packageName = "kr.genti.android"
+
     const val compileSdk = 34
     const val minSdk = 28
     const val targetSdk = 34
+
     const val versionCode = 18
     const val versionName = "2.0.2"
+
     const val jvmVersion = "17"
 }
---
+++ build-logic/convention/src/main/java/kr/genti/convention/config/CommonPluginConfig.kt
@@ -0,0 +1,22 @@
+package kr.genti.convention.config
+
+import kr.genti.convention.extension.getLibrary
+import kr.genti.convention.extension.implementation
+import org.gradle.api.Project
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.getByType
+
+fun Project.configureAndroidCommonPlugin() {
+    apply<AndroidKotlinPlugin>()
+    apply<AndroidHiltPlugin>()
+    with(plugins) {
+        apply("kotlin-kapt")
+        apply("kotlin-parcelize")
+    }
+
+    val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+    dependencies {
+        implementation(libs.getLibrary("materialDesign"))
+    }
