bc0712aaa1580079358ad73fd7a3eb381bd78b4f
Sangho Kim <marchbreeze03@gmail.com>
2024-11-30 01:56:09 +0900

    [ADD/#178] keystoreProperties gradle 설정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,5 @@
 import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+import java.util.Properties
 
 plugins {
     id("kr.genti.androidApplication")
@@ -16,6 +17,19 @@ android {
 
         manifestPlaceholders["NATIVE_APP_KEY"] =
             gradleLocalProperties(rootDir).getProperty("nativeAppKey")
+
+        val keystorePropertiesFile = rootProject.file("keystore.properties")
+        val keystoreProperties = Properties()
+        keystoreProperties.load(keystorePropertiesFile.inputStream())
+
+        signingConfigs {
+            create("release") {
+                storeFile = file(keystoreProperties["storeFile"] as String)
+                storePassword = keystoreProperties["storePassword"] as String
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+            }
+        }
     }
 
     buildTypes {
